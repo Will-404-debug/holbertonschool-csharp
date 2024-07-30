4316f552b42d1ef2c04ae731242871cbb2373bcb
William <wilguidro@gmail.com>
2024-07-30 17:18:57 +0200

    Build new project

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#Define the project name and directory
+# Define the project name and directory
 PROJECT_NAME="1-new_project"
 PROJECT_DIR="./${PROJECT_NAME}"
 
@@ -8,15 +8,13 @@ PROJECT_DIR="./${PROJECT_NAME}"
 mkdir -p "$PROJECT_DIR"
 
 # Change to the project directory
-cd "$PROJECT_DIR" || { echo "Failed to change directory to $PROJECT_DIR"; exit 1; }
+cd "$PROJECT_DIR" || exit 1
 
 # Initialize a new C# project
-dotnet new console -o . || { echo "Failed to initialize the C# project"; exit 1; }
+dotnet new console -o . || exit 1
 
 # Restore the project dependencies
-dotnet restore || { echo "Failed to restore project dependencies"; exit 1; }
+dotnet restore || exit 1
 
 # Build the project
-dotnet build || { echo "Failed to build the project"; exit 1; }
-
-echo "Project initialized and built successfully."
+dotnet build
