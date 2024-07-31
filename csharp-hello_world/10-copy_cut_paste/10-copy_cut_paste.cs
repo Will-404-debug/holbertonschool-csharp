a4df6d675e6062a0284eadcc7e8f3a8ca86847a9
William <wilguidro@gmail.com>
2024-07-31 13:43:33 +0200

    Copy - Cut - Paste

---
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+		string word = "Holberton";
+		string word_first_3 = word.Substring(0, 3); // Extract first 3 letters
+		string word_last_2 = word.Substring(word.Length - 2); // Extract last 2 letters
+                string middle_word = word.Substring(1, word.Length - 2); // Extract middle letters
+		Console.WriteLine("First 3 letters: {0}", word_first_3);
+                Console.WriteLine("Last 2 letters: {0}", word_last_2);
+                Console.WriteLine("Middle word: {0}", middle_word);
+        }
+}
