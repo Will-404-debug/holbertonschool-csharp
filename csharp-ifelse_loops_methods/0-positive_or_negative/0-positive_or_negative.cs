b4e4c36405dea5bc5e7e60368dfc53217ad868cc
William <wilguidro@gmail.com>
2024-08-01 15:51:13 +0200

    Positive anything is better than negative nothing

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        
+        if (number > 0)
+        {
+            Console.WriteLine($"{number} is positive");
+        }
+        else if (number == 0)
+        {
+            Console.WriteLine($"{number} is zero");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is negative");
+        }
+    }
+}
---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/README.md
@@ -0,0 +1,9 @@
+# 0-positive_or_negative
+
+This project contains a C# console application that assigns a random signed number to a variable and prints whether the number is positive, negative, or zero.
+
+## How to Run
+
+1. Navigate to the project folder:
+   ```sh
+   cd holbertonschool-csharp/csharp-ifelse_loops_methods/0-positive_or_negative
