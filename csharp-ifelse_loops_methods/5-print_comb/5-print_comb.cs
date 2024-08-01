25ccad62b188cc617657a5798f4fb8d0135a2c86
William <wilguidro@gmail.com>
2024-08-01 23:59:27 +0200

    00...99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i <= 99; i++)
+		{
+			if (i < 99)
+			{
+				Console.Write($"{i:00}, ");
+			}
+			else
+			{
+				Console.WriteLine($"{i:00}");
+			}
+		}
+	}
+}
