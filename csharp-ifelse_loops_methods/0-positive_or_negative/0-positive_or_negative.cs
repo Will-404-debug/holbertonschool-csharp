a72a2e2d95ffbb2b765715de4f6df4db6414c457
William <wilguidro@gmail.com>
2024-08-01 19:06:42 +0200

    Positive anything is better than negative nothing.update

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -2,22 +2,22 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Random rndm = new Random();
-        int number = rndm.Next(-10, 10);
-        
-        if (number > 0)
-        {
-            Console.WriteLine($"{number} is positive");
-        }
-        else if (number == 0)
-        {
-            Console.WriteLine($"{number} is zero");
-        }
-        else
-        {
-            Console.WriteLine($"{number} is negative");
-        }
-    }
+	static void Main(string[] args)
+	{
+		Random rndm = new Random();
+		int number = rndm.Next(-10, 10);
+						        
+		if (number > 0)
+		{
+			Console.WriteLine($"{number} is positive");
+		}
+		else if (number == 0)
+		{
+			Console.WriteLine($"{number} is zero");
+		}
+		else
+		{
+			Console.WriteLine($"{number} is negative");
+		}
+	}
 }
