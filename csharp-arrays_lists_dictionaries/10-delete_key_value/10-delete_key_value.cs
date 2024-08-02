dcd6abd585088d52bc3606b27c5598ac0cdd66a1
William <wilguidro@gmail.com>
2024-08-02 17:51:38 +0200

    Delete from dictionary

---
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+	{
+		// Check if the key exists in the dictionary and remove it if it does
+		if (myDict.ContainsKey(key))
+		{
+			myDict.Remove(key);
+		}
+
+		// Return the updated dictionary
+		return myDict;
+	}
+}
