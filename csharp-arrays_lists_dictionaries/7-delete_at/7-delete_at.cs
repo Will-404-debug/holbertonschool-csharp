8d91b56001b0d1c7ccceae96002fde2aff07c60c
William <wilguidro@gmail.com>
2024-08-02 15:01:37 +0200

    Delete at

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> DeleteAt(List<int> myList, int index)
+	{
+		// Check if the index is out of range
+		if (index < 0 || index >= myList.Count)
+		{
+			Console.WriteLine("Index is out of range");
+			return myList;
+		}
+
+		// Create a new list excluding the element at the given index
+		List<int> newList = new List<int>();
+
+		for (int i = 0; i < myList.Count; i++)
+		{
+			if (i != index)
+			{
+				newList.Add(myList[i]);
+			}
+		}
+		
+		return newList;
+	}
+}
