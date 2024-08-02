d923e69bfac01605d81b2bbcd2d016221671bffa
William <wilguidro@gmail.com>
2024-08-02 16:20:05 +0200

    Update dictionary

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+	{
+		// Add or update the key-value pair in the dictionary
+		myDict[key] = value;
+
+		// Return the updated dictionary
+		return myDict;
+	}
+}
