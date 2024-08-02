4ede538a408b5e57f940870261ab3ed5d767e64e
William <wilguidro@gmail.com>
2024-08-02 13:20:30 +0200

    Replace element

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-main.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[] array = { 0, 1, 2, 3, 4, 5, 6 };
+
+		PrintArray(array);
+		Array.ReplaceElement(array, 1, 98);
+		PrintArray(array);
+	}
+	
+	static void PrintArray(int[] array)
+	{
+		for (int i = 0; i < array.Length; i++)
+		{
+			Console.Write(array[i]);
+			if (i < array.Length - 1)
+				Console.Write(" ");
+		}
+
+	        Console.WriteLine();
+	}
+}
