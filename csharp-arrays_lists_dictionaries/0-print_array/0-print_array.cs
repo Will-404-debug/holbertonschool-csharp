e59f1a6da9c9bd6e35c8db72bd6bdc9d06def76c
William <wilguidro@gmail.com>
2024-08-02 11:53:11 +0200

    Print an array of integers.update

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -10,6 +10,12 @@ class Array
 			return null;
 		}
 		
+		if (size == 0)
+		{
+			Console.WriteLine();
+			return new int[0];
+		}
+
 		int[] newArray = new int[size];
 
 		for (int i = 0; i < size; i++)
