c50b1f413630174eb44523ba00bce88af9bbac19
William <wilguidro@gmail.com>
2024-08-07 00:25:09 +0200

    Present in both

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+	{
+		HashSet<int> set1 = new HashSet<int>(list1);
+		HashSet<int> commonSet = new HashSet<int>();
+		
+		foreach (int num in list2)
+		{
+			if (set1.Contains(num))
+			{
+				commonSet.Add(num);
+			}
+		}
+		
+		List<int> commonList = new List<int>(commonSet);
+		commonList.Sort(); // Manual sorting can also be implemented if required
+
+		return commonList;
+	}
+}
