2749312ad06a648346d66b10a7ef4d4cc257e1cd
William <wilguidro@gmail.com>
2024-08-06 22:45:40 +0200

    Unique addition

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		HashSet<int> uniqueIntegers = new HashSet<int>(myList);
+		int sum = 0;
+		foreach (int num in uniqueIntegers)
+		{
+			sum += num;
+		}
+		return sum;
+	}
+}
---
+++ csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,9 @@
+### Main Project README.md
+```markdown
+# csharp-hashset_stack_queue_linkedlist
+
+This repository contains various C# projects involving hashsets, stacks, queues, and linked lists.
+
+## Projects
+
+- [0-unique_add](0-unique_add)
