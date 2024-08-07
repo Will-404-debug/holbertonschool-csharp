5c3a5f52429c3793d47f75b28bca4e2a98829c40
William <wilguidro@gmail.com>
2024-08-07 11:54:41 +0200

    Only differents

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> DifferentElements(List<int> list1, List<int> list2)
+	{
+		HashSet<int> set1 = new HashSet<int>(list1);
+		HashSet<int> set2 = new HashSet<int>(list2);
+	       	HashSet<int> uniqueSet = new HashSet<int>();
+
+		foreach (int num in list1)
+		{
+			if (!set2.Contains(num))
+			{
+				uniqueSet.Add(num);
+			}
+		}
+
+		foreach (int num in list2)
+		{
+			if (!set1.Contains(num))
+			{
+				uniqueSet.Add(num);
+			}
+		}
+
+		List<int> differentList = new List<int>(uniqueSet);
+		differentList.Sort(); // Manual sorting can also be implemented if required
+
+		return differentList;
+	}
+}
