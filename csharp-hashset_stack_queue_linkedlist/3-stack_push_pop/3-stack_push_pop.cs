03007706493c5339ac38a4b9498a068be129acab
William <wilguidro@gmail.com>
2024-08-08 10:34:49 +0200

    Full Stack

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class MyStack
+public class MyStack
 {
 	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
 	{
