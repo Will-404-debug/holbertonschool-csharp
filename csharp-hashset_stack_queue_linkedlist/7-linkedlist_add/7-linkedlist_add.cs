d9a996faeee49141800731973f30a81cad72a019
William <wilguidro@gmail.com>
2024-08-08 20:56:55 +0200

    Add node to the beginning

---
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+	{
+		// Add a new node with value n at the beginning of the linked list
+		myLList.AddFirst(n);
+
+		// Return the newly added node, which is now the first node
+		return myLList.First;
+	}
+}
