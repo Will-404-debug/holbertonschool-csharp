19bb0815893f714d6b3e90c0005993ac27e1260d
William <wilguidro@gmail.com>
2024-08-09 00:14:26 +0200

    Delete at index

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static void Delete(LinkedList<int> myLList, int index)
+	{
+		// If the list is empty, return
+		if (myLList.First == null)
+			return;
+
+		// Traverse the list to find the node at the specified index
+		LinkedListNode<int> current = myLList.First;
+		int currentIndex = 0;
+
+		// Special case for deleting the first node
+		if (index == 0)
+		{
+			myLList.RemoveFirst();
+			return;
+		}
+
+		// Traverse the list to find the node at the specified index
+		while (current != null && currentIndex < index)
+		{
+			current = current.Next;
+			currentIndex++;
+		}
+
+		// If the node at the specified index exists, remove it
+		if (current != null)
+		{
+			myLList.Remove(current);
+		}
+	}
+}
