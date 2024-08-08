cff6288ea9dc9273f7fb8723f8343c60f9b3ed10
William <wilguidro@gmail.com>
2024-08-08 19:53:05 +0200

    Print a LinkedList

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+		// Create a new LinkedList of integers
+		LinkedList<int> linkedList = new LinkedList<int>();
+
+		// Check if the size is non-negative
+		if (size < 0)
+		{
+			return linkedList; // Return an empty list if size is negative
+		}
+
+		// Populate the LinkedList with integers from 0 to size-1
+		for (int i = 0; i < size; i++)
+		{
+			linkedList.AddLast(i);
+			Console.WriteLine(i); // Print the current integer
+		}
+
+		// Return the created LinkedList
+		return linkedList;
+	}
+}
