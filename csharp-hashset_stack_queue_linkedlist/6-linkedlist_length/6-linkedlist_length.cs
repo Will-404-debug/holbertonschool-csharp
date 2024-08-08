afe1b43910837955a2dc4f1348184345f1480adb
William <wilguidro@gmail.com>
2024-08-08 20:39:21 +0200

    LinkedList length

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Length(LinkedList<int> myLList)
+	{
+		int count = 0;
+
+		// Iterate through the linked list
+		foreach (int item in myLList)
+		{
+			count++; // Increment the count for each item
+		}
+
+		return count; // Return the total count
+	}
+}
