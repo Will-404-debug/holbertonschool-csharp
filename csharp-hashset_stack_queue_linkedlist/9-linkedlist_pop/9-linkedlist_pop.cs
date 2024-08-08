47127904b0bcfba6bc8ce15441c5894f36d5d067
William <wilguidro@gmail.com>
2024-08-08 21:57:00 +0200

    Pop

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Pop(LinkedList<int> myLList)
+	{
+		// Check if the linked list is empty
+		if (myLList.First == null)
+		{
+			return 0; // Return 0 if the list is empty
+		}
+
+		// Get the value of the head node
+		int headValue = myLList.First.Value;
+
+		// Remove the head node
+		myLList.RemoveFirst();
+
+		// Return the value of the removed node
+		return headValue;
+	}
+}
