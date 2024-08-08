66f71c5eb411ff2f1b5320b3f415426a701274ba
William <wilguidro@gmail.com>
2024-08-08 23:52:32 +0200

    Insert in order

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		// Create a new node
+		LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+
+		// If the list is empty, add the new node as the first node
+		if (myLList.First == null)
+		{
+			myLList.AddFirst(newNode);
+			return newNode;
+		}
+
+		// Traverse the list to find the correct position
+		LinkedListNode<int> current = myLList.First;
+		while (current != null)
+		{
+			// If the new value should be inserted before the current node
+			if (n < current.Value)
+			{
+				myLList.AddBefore(current, newNode);
+				return newNode;
+			}
+			current = current.Next;
+		}
+
+		// If the new value is greater than all existing values, add it at the end
+		myLList.AddLast(newNode);
+		return newNode;
+	}
+}
