29aba2a30e4fd399544cb40fa98dc6409165313d
William <wilguidro@gmail.com>
2024-09-16 14:57:39 +0200

    Chief Puppy Officer

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public struct Dog
+{
+	public string name;   // Dog's name
+	public float age;     // Dog's age
+        public string owner;  // Dog's owner
+	public Rating rating; // Dog's rating
+}
+
+	public enum Rating
+{
+	Good,       // 0
+	Great,      // 1
+	Excellent   // 2
+}
