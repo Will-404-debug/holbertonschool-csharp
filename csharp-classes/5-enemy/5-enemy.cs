64b7cc6920c4269f294b08c24cf7214061a8dcdb
William <wilguidro@gmail.com>
2024-09-23 15:34:45 +0200

    Printing a zombie

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Enemies
+{
+	/// <summary>
+	/// Represents a zombie enemy.
+	/// </summary>
+	public class Zombie
+	{
+		/// <summary>
+		/// The health of the zombie (private).
+		/// </summary>
+		private int health;
+
+		/// <summary>
+		/// The name of the zombie (private). Defaults to "(No name)".
+		/// </summary>
+		private string name = "(No name)";
+
+		/// <summary>
+		/// Gets or sets the name of the zombie.
+		/// </summary>
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the Zombie class with health set to 0.
+		/// </summary>
+		public Zombie()
+		{
+			health = 0;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the Zombie class with a specified health value.
+		/// </summary>
+		/// <param name="value">The health value to set.</param>
+		/// <exception cref="ArgumentException">Thrown when value is less than 0.</exception>
+		public Zombie(int value)
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+			health = value;
+		}
+
+		/// <summary>
+		/// Returns the health of the zombie.
+		/// </summary>
+		/// <returns>The health value of the zombie.</returns>
+		public int GetHealth()
+		{
+			return health;
+		}
+
+		/// <summary>
+		/// Overrides the ToString method to return a string representation of the zombie's name and health.
+		/// </summary>
+		/// <returns>A string describing the zombie.</returns>
+		public override string ToString()
+		{
+			return $"Zombie Name: {name} / Total Health: {health}";
+		}
+	}
+}
