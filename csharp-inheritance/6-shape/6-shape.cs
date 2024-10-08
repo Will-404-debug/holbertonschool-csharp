120750e8f64b732672d49bbcda43a0b41656a185
William <wilguidro@gmail.com>
2024-10-08 22:34:52 +0200

    Rectangle subclass.updated

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -7,10 +7,9 @@
 class Shape
 {
 	/// <summary>
-	/// Calculates the area of the shape.
+	/// Virtual method Area which throws a NotImplementedException.
 	/// </summary>
-	/// <returns>The area of the shape.</returns>
-	/// <exception cref="NotImplementedException">Thrown when the method is not implemented.</exception>
+	/// <returns>Throws NotImplementedException</returns>
 	public virtual int Area()
 	{
 		throw new NotImplementedException("Area() is not implemented");
