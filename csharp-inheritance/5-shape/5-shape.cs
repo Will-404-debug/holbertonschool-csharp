86623eaf5b4aaa8e3b17de09e19730a56a9c9bf1
William <wilguidro@gmail.com>
2024-10-08 16:40:47 +0200

    Basic shapes

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Represents a generic shape.
+/// </summary>
+public class Shape
+{
+	/// <summary>
+	/// Calculates the area of the shape.
+	/// </summary>
+	/// <returns>Throws NotImplementedException.</returns>
+	public virtual int Area()
+	{
+		throw new NotImplementedException("Area() is not implemented");
+	}
+}
