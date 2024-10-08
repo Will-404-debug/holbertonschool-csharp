4a0e185fdcc617c381de43bb9f82805e5758d4df
William <wilguidro@gmail.com>
2024-10-08 15:53:24 +0200

    Type

---
+++ csharp-inheritance/3-type_get/3-main.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var num = 10;
+		var myList = new List<int>();
+
+		Obj.Print(num);
+		Console.WriteLine("-----------------");
+		Obj.Print(myList);
+	}
+}
---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,39 @@
+﻿// 3-type_get.cs
+using System;
+using System.Reflection;
+
+/// <summary>
+/// This class provides methods for inspecting the properties and methods of an object.
+/// </summary>
+public class Obj
+{
+	/// <summary>
+	/// Prints the names of the available properties and methods of an object.
+	/// </summary>
+	/// <param name="myObj">The object to inspect.</param>
+	public static void Print(object myObj)
+	{
+		// Get the Type of the object
+		Type objType = myObj.GetType();
+
+		// Print the name of the class and its properties
+		Console.WriteLine($"{objType.Name} Properties:");
+
+		// Get and print all properties
+		PropertyInfo[] properties = objType.GetProperties();
+		foreach (var prop in properties)
+		{
+			Console.WriteLine(prop.Name);
+		}
+
+		// Print the name of the class and its methods
+		Console.WriteLine($"{objType.Name} Methods:");
+
+		// Get and print all methods
+		MethodInfo[] methods = objType.GetMethods();
+		foreach (var method in methods)
+		{
+			Console.WriteLine(method.Name);
+		}
+	}
+}
