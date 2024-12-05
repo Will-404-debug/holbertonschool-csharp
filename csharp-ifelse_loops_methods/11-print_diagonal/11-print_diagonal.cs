887ef17369fbeb1bf3170b3c96cfa843b123ccef
William <wilguidro@gmail.com>
2024-12-05 10:19:48 +0100

    Update 11-print_diagonal.cs

--- csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -1,20 +1,20 @@
-﻿using System;
+using System;
 
 class Line
 {
-	public static void PrintDiagonal(int length)
-	{
-		if (length <= 0)
-		{
-			Console.WriteLine();
-			return;
-		}
-
-		for (int i = 0; i < length; i++)
-		{
-			Console.Write(new string(' ', i)); // Print leading spaces
-			Console.Write("\\"); // Print the diagonal line character
-			Console.WriteLine(); // Move to the next line
-		}
-	}
+    public static void PrintDiagonal(int length)
+    {
+        if (length < 0)
+            Console.WriteLine();
+        else
+        {
+            string emptySpace = "";
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(emptySpace + "\\");
+                emptySpace += " ";
+            }
+            Console.WriteLine();
+        }
+    }
 }
