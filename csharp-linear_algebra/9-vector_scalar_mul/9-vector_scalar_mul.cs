f16320e7f8aeb4ae8133fbccdb883251cc375faf
William <wilguidro@gmail.com>
2025-01-27 18:54:36 +0100

    Created the method that multiplies a vector and a scalar and returns the resulting vector

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Class for vector math
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Multiply a 2d / 3d vector by a scalar
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <param name="scalar"></param>
+    /// <returns></returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+            return new double[] { -1 };
+        double[] result = new double[vector.Length];
+
+        for (int i = 0; i < vector.Length; i++)
+            result[i] = vector[i] * scalar;
+        return result;
+    }
+}
