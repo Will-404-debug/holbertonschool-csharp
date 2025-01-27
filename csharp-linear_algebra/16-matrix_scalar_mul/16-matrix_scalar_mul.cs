ba446183babda95e05c3a1e690cf3bca24df7cae
William <wilguidro@gmail.com>
2025-01-27 19:26:17 +0100

    Created a method that multiplies a matrix and a scalar and returns the resulting matrix.

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/// <summary>
+/// class for matrix math
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Multiply a matrix by a scalar
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="scalar"></param>
+    /// <returns></returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if ((matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3) || (matrix.GetLength(1) != 2 && matrix.GetLength(1) != 3))
+            return new double[,] { { -1 } };
+        
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int y = 0; y < matrix.GetLength(1); y++)
+                result[i, y] = matrix[i, y] * scalar;
+        return result;
+    }
+}
