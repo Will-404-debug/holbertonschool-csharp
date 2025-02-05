cfa76c88f2f512ace0b24afee0e3a7def41b8e97
William <wilguidro@gmail.com>
2025-02-05 13:05:02 +0100

    Image Processor for inverting colors implemented

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+class ImageProcessor
+{
+    public static void Inverse(string[] filenames)
+    {
+        Parallel.ForEach(filenames, filename =>
+        {
+            try
+            {
+                using (Image<Rgba32> image = Image.Load<Rgba32>(filename))
+                {
+                    image.Mutate(ctx => ctx.Invert()); // Apply color inversion
+
+                    string newFilename = Path.Combine(
+                        AppDomain.CurrentDomain.BaseDirectory,
+                        Path.GetFileNameWithoutExtension(filename) + "_inverse" + Path.GetExtension(filename)
+                    );
+
+                    image.Save(newFilename); // Save with the same format
+                    Console.WriteLine($"Saved inverted image: {newFilename}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing {filename}: {ex.Message}");
+            }
+        });
+    }
+}
