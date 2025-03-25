48151dca3550e146c07bdd2e362f7ccbece27868
William <wilguidro@gmail.com>
2025-03-25 16:16:49 +0100

    Task 1 Enqueue

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+/// <summary>
+/// Custom generic Queue class
+/// </summary>
+public class Queue<T>
+{
+    /// <summary>
+    /// Node class inside Queue
+    /// </summary>
+    public class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T val)
+        {
+            value = val;
+            next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+    public int count;
+
+    /// <summary>
+    /// Adds a new node at the end of the queue
+    /// </summary>
+    public void Enqueue(T item)
+    {
+        Node newNode = new Node(item);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    /// <summary>
+    /// Returns the number of nodes in the queue
+    /// </summary>
+    public int Count()
+    {
+        return count;
+    }
+
+    /// <summary>
+    /// Returns the type of the Queue
+    /// </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
