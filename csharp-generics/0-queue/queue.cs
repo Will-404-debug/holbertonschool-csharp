1eeb6d72d941fffb57081a13a0ff4e0817056f1b
William <wilguidro@gmail.com>
2025-03-25 13:10:00 +0100

    First task Queue

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Generic Queue class without using System.Collections
+/// </summary>
+public class Queue<T>
+{
+    /// <summary>
+    /// Returns the type of the generic parameter T
+    /// </summary>
+    /// <returns>Type of T</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
