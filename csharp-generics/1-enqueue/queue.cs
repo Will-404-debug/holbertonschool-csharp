4dfc8233575061f3f2927e5762fcdc6e3ca89d03
William <wilguidro@gmail.com>
2025-03-25 16:28:54 +0100

    Review code

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -1,35 +1,46 @@
 ﻿using System;
 
-/// <summary>
-/// Custom generic Queue class
-/// </summary>
-public class Queue<T>
+/// <summary>Generic Queue class</summary>
+class Queue<T>
 {
-    /// <summary>
-    /// Node class inside Queue
-    /// </summary>
+    /// <summary>Head of the queue</summary>
+    public Node head = null;
+
+    /// <summary>Tail of the queue</summary>
+    public Node tail = null;
+
+    /// <summary>Number of elements in the queue</summary>
+    public int count = 0;
+
+    /// <summary>Node class representing each element in the queue</summary>
     public class Node
     {
-        public T value;
-        public Node next;
+        /// <summary>Value stored in the node</summary>
+        public T value = default(T);
 
-        public Node(T val)
+        /// <summary>Reference to the next node</summary>
+        public Node next = null;
+
+        /// <summary>Constructor to initialize node with a value</summary>
+        public Node(T value)
         {
-            value = val;
-            next = null;
+            this.value = value;
+            this.next = null;
         }
     }
 
-    public Node head;
-    public Node tail;
-    public int count;
-
-    /// <summary>
-    /// Adds a new node at the end of the queue
-    /// </summary>
-    public void Enqueue(T item)
+    /// <summary>Returns the type of objects stored in the queue</summary>
+    /// <returns>The Type of T</returns>
+    public Type CheckType()
     {
-        Node newNode = new Node(item);
+        return typeof(T);
+    }
+
+    /// <summary>Adds a new node containing value to the end of the queue</summary>
+    /// <param name="value">Value to enqueue</param>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
 
         if (head == null)
         {
@@ -45,19 +56,10 @@ public class Queue<T>
         count++;
     }
 
-    /// <summary>
-    /// Returns the number of nodes in the queue
-    /// </summary>
+    /// <summary>Returns the number of nodes in the queue</summary>
+    /// <returns>Count of nodes</returns>
     public int Count()
     {
         return count;
-    }
-
-    /// <summary>
-    /// Returns the type of the Queue
-    /// </summary>
-    public Type CheckType()
-    {
-        return typeof(T);
     }
 }
