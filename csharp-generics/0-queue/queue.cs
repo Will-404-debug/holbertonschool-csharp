6c4d2ccae1cd8224b150567a01d709bb66b15694
William <wilguidro@gmail.com>
2025-03-25 13:33:31 +0100

    Task 0 Queue

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Custom generic Queue class
+/// </summary>
+public class Queue<T>
+{
+    /// <summary>
+    /// Returns the type of T
+    /// </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
