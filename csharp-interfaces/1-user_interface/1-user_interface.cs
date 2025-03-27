fc61fee9010258b9c8944b854e9abe4bd8dd2c19
William <wilguidro@gmail.com>
2025-03-27 12:50:20 +0100

    Create interface

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 
-namespace _1_user_interface
+/// <summary>
+/// Base class
+/// </summary>
+abstract class Base
 {
-    class Program
+    /// <summary>Name property</summary>
+    public string name { get; set; }
+
+    /// <summary>ToString override</summary>
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+/// <summary>Interface for interactive objects</summary>
+public interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>Interface for breakable objects</summary>
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>Interface for collectable objects</summary>
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>
+/// TestObject class that inherits Base and implements
+/// IInteractive, IBreakable, and ICollectable
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+        // Not implemented
+    }
+
+    public void Break()
+    {
+        // Not implemented
+    }
+
+    public void Collect()
+    {
+        // Not implemented
     }
 }
