176b5d2d35478bc80aa8079137044cc46ad1b4da
William <wilguidro@gmail.com>
2025-03-27 15:33:22 +0100

    Create class RoomObjects and method IterateAction

--- csharp-interfaces/5-iterate_act/5-iterate_act.cs
+++ csharp-interfaces/5-iterate_act/5-iterate_act.cs
@@ -1,12 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _5_iterate_act
+/// <summary>Base class</summary>
+abstract class Base
 {
-    class Program
+    public string name { get; set; }
+
+    public override string ToString()
     {
-        static void Main(string[] args)
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+/// <summary>Interactive interface</summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>Breakable interface</summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>Collectable interface</summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>Door class</summary>
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
+/// <summary>Decoration class</summary>
+class Decoration : Base, IInteractive, IBreakable
+{
+    public int durability { get; set; }
+    public bool isQuestItem;
+
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+            throw new Exception("Durability must be greater than 0");
+
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    public void Interact()
+    {
+        if (durability <= 0)
+            Console.WriteLine($"The {name} has been broken.");
+        else if (isQuestItem)
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        else
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+    }
+
+    public void Break()
+    {
+        durability--;
+        if (durability > 0)
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        else if (durability == 0)
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        else
+            Console.WriteLine($"The {name} is already broken.");
+    }
+}
+
+/// <summary>Key class</summary>
+class Key : Base, ICollectable
+{
+    public bool isCollected { get; set; }
+
+    public Key(string name = "Key", bool isCollected = false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    public void Collect()
+    {
+        if (!isCollected)
         {
-            Console.WriteLine("Hello World!");
+            isCollected = true;
+            Console.WriteLine($"You pick up the {name}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have already picked up the {name}.");
+        }
+    }
+}
+
+/// <summary>RoomObjects helper class</summary>
+class RoomObjects
+{
+    public static void IterateAction(List<Base> roomObjects, Type type)
+    {
+        foreach (Base obj in roomObjects)
+        {
+            if (type == typeof(IInteractive) && obj is IInteractive interactive)
+                interactive.Interact();
+
+            else if (type == typeof(IBreakable) && obj is IBreakable breakable)
+                breakable.Break();
+
+            else if (type == typeof(ICollectable) && obj is ICollectable collectable)
+                collectable.Collect();
         }
     }
 }
