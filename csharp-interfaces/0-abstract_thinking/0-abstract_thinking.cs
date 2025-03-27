4efa42d0a6358e3f9357f17a1a7e1b8a0c5e214b
William <wilguidro@gmail.com>
2025-03-27 11:49:50 +0100

    Create abstract class and project directories and files

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Abstract class Base with a public property 'name'
+/// and an overridden ToString method.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public string property
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Override ToString to return "<name> is a <type>"
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType().Name}";
+    }
+}
