7ec27e7977d346fe7758ded68c4bdd490d940311
William <wilguidro@gmail.com>
2025-03-27 14:25:53 +0100

    Create class Decorations

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 
-/// <summary>
-/// Base class
-/// </summary>
+/// <summary>Base class</summary>
 abstract class Base
 {
     /// <summary>Name property</summary>
@@ -11,7 +9,7 @@ abstract class Base
     /// <summary>ToString override</summary>
     public override string ToString()
     {
-        return $"{name} is a {GetType().Name}";
+        return $"{name} is a {this.GetType().Name}";
     }
 }
 
@@ -21,18 +19,66 @@ interface IInteractive
     void Interact();
 }
 
-/// <summary>Door class that inherits Base and IInteractive</summary>
-class Door : Base, IInteractive
+/// <summary>Interface for breakable objects</summary>
+interface IBreakable
 {
-    /// <summary>Constructor sets name (default = "Door")</summary>
-    public Door(string name = "Door")
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>Decoration class</summary>
+class Decoration : Base, IInteractive, IBreakable
+{
+    /// <summary>Durability of the object</summary>
+    public int durability { get; set; }
+
+    /// <summary>Whether it’s a quest item</summary>
+    public bool isQuestItem;
+
+    /// <summary>Constructor for Decoration</summary>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
     {
+        if (durability <= 0)
+            throw new Exception("Durability must be greater than 0");
+
         this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
     }
 
     /// <summary>Interact implementation</summary>
     public void Interact()
     {
-        Console.WriteLine($"You try to open the {name}. It's locked.");
+        if (durability <= 0)
+        {
+            Console.WriteLine($"The {name} has been broken.");
+        }
+        else if (isQuestItem)
+        {
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        }
+        else
+        {
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+        }
+    }
+
+    /// <summary>Break implementation</summary>
+    public void Break()
+    {
+        durability--;
+
+        if (durability > 0)
+        {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        }
+        else if (durability == 0)
+        {
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        }
+        else
+        {
+            Console.WriteLine($"The {name} is already broken.");
+        }
     }
 }
