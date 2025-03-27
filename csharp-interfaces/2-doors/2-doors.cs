f1a70a5203a0451f2a38b368d01ad63eaef047f3
William <wilguidro@gmail.com>
2025-03-27 13:54:35 +0100

    The Door

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace _2_doors
+/// <summary>
+/// Base class
+/// </summary>
+abstract class Base
 {
-    class Program
+    /// <summary>Name property</summary>
+    public string name { get; set; }
+
+    /// <summary>ToString override</summary>
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+/// <summary>Interface for interactive objects</summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>Door class that inherits Base and IInteractive</summary>
+class Door : Base, IInteractive
+{
+    /// <summary>Constructor sets name (default = "Door")</summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    /// <summary>Interact implementation</summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
     }
 }
