e1bf3c79aa5dfcb1faf5b2ac4b66b55982a5850d
William <wilguidro@gmail.com>
2025-03-28 13:47:35 +0100

    Calculate the new value of the Player’s hp

--- csharp-delegates_events/2-validation/2-validation.cs
+++ csharp-delegates_events/2-validation/2-validation.cs
@@ -1,12 +1,74 @@
 ﻿using System;
 
-namespace _2_validation
+/// <summary> Delegate that takes a float amount. </summary>
+public delegate void CalculateHealth(float amount);
+
+class Player
 {
-    class Program
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    // Constructor
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        this.name = name;
+
+        if (maxHp <= 0f)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.hp = this.maxHp;
+    }
+
+    // PrintHealth method
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    // TakeDamage method
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+            damage = 0f;
+
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+        float newHp = this.hp - damage;
+        ValidateHP(newHp);
+    }
+
+    // HealDamage method
+    public void HealDamage(float heal)
+    {
+        if (heal < 0f)
+            heal = 0f;
+
+        Console.WriteLine($"{this.name} heals {heal} HP!");
+        float newHp = this.hp + heal;
+        ValidateHP(newHp);
+    }
+
+    // ValidateHP method
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0f)
+        {
+            this.hp = 0f;
+        }
+        else if (newHp > this.maxHp)
+        {
+            this.hp = this.maxHp;
+        }
+        else
+        {
+            this.hp = newHp;
         }
     }
 }
