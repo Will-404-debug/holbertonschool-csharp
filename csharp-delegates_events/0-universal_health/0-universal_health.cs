58b3070bce604901a186fb7f9378af768a869b79
William <wilguidro@gmail.com>
2025-03-28 13:14:45 +0100

    Class Player

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace _0_universal_health
+public class Player
 {
-    class Program
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    // Constructor
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        this.name = name;
+
+        if (maxHp <= 0f)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
         }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.hp = this.maxHp;
+    }
+
+    // PrintHealth method
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
     }
 }
