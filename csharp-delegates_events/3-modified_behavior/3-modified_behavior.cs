72201c8efebb724cba7041cb1c127857035334eb
William <wilguidro@gmail.com>
2025-03-28 14:00:25 +0100

    Create an enum Modifier with the values Weak, Base, Strong

--- csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
+++ csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -1,12 +1,96 @@
 ﻿using System;
 
-namespace _3_modified_behavior
+/// <summary> Modifier enum. </summary>
+public enum Modifier
 {
-    class Program
+    Weak,
+    Base,
+    Strong
+}
+
+/// <summary> Delegate for calculating a modified value. </summary>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    // Constructor
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        this.name = name;
+
+        if (maxHp <= 0f)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.hp = this.maxHp;
+    }
+
+    // PrintHealth method
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    // TakeDamage method
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+            damage = 0f;
+
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+        float newHp = this.hp - damage;
+        ValidateHP(newHp);
+    }
+
+    // HealDamage method
+    public void HealDamage(float heal)
+    {
+        if (heal < 0f)
+            heal = 0f;
+
+        Console.WriteLine($"{this.name} heals {heal} HP!");
+        float newHp = this.hp + heal;
+        ValidateHP(newHp);
+    }
+
+    // ValidateHP method
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0f)
+        {
+            this.hp = 0f;
+        }
+        else if (newHp > this.maxHp)
+        {
+            this.hp = this.maxHp;
+        }
+        else
+        {
+            this.hp = newHp;
+        }
+    }
+
+    // ApplyModifier method matching the delegate
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        switch (modifier)
+        {
+            case Modifier.Weak:
+                return baseValue * 0.5f;
+            case Modifier.Strong:
+                return baseValue * 1.5f;
+            default:
+                return baseValue;
         }
     }
 }
